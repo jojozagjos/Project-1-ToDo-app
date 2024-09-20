c205330372de797f0ce0b10c5b2243bac58d4bb5
jojozagjos <josephgslade@gmail.com>
2024-09-20 07:49:23 -0600

    finished the website with all features working

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Project-1-ToDo-app
+
+## Overview
+This website is a simple and interactive To-Do List application that allows users to create, manage, and delete multiple to-do lists. Each list can have its own title and individual to-do items. Users can mark items as completed, add new items, and delete entire lists with a confirmation prompt to prevent accidental deletions.
+
+## Features
+- **Create New Lists**: Users can create new to-do lists by entering a title and clicking the "Add List" button.
+- **Add To-Do Items**: Each list has an input field to add new to-do items. Users can type the item and click the "Add" button to add it to the list.
+- **Mark Items as Completed**: Users can click on any to-do item to mark it as completed. Completed items will have a strikethrough effect.
+- **Delete Items**: Each to-do item has a close button (X) that allows users to delete the item.
+- **Delete Lists**: Each list has a delete button (X) that allows users to delete the entire list. A confirmation prompt ensures that users do not accidentally delete a list.
+
+## Technologies Used
+- **HTML**: For the structure of the website.
+- **CSS**: For styling the website and making it visually appealing.
+- **JavaScript**: For adding interactivity and functionality to the website.
+
+## How to Use
+1. **Create a New List**: Enter a title in the input field at the top of the page and click the "Add List" button. A new list with the given title will be created.
+2. **Add To-Do Items**: In each list, enter a to-do item in the input field and click the "Add" button. The item will be added to the list.
+3. **Mark Items as Completed**: Click on any to-do item to mark it as completed. The item will have a strikethrough effect.
+4. **Delete Items**: Click the close button (X) next to any to-do item to delete it.
+5. **Delete Lists**: Click the delete button (X) at the top of any list to delete the entire list. A confirmation prompt will appear to confirm the deletion.
+
+## File Structure
+- **index.html**: The main HTML file that contains the structure of the website.
+- **CSS/styles.css**: The CSS file that contains the styles for the website.
+- **Script/script.js**: The JavaScript file that contains the functionality and interactivity for the website.
--- Script/script.js
+++ Script/script.js
@@ -1,51 +1,87 @@
 var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+for (var i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
 }
 
 var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+var listsContainer = document.getElementById('listsContainer');
+listsContainer.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
 }, false);
 
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+function addList() {
+    const listTitle = document.getElementById('ListInput').value;
+    if (listTitle === '') {
+        alert('Please enter a title for the list.');
+        return;
     }
-  }
+
+    const listsContainer = document.getElementById('listsContainer');
+    const newList = document.createElement('div');
+    newList.className = 'container';
+
+    newList.innerHTML = `
+        <div class="header">
+            <h2>${listTitle}</h2>
+            <input type="text" placeholder="Title...">
+            <span onclick="addItem(this)" class="addBtn">Add</span>
+        </div>
+        <ul></ul>
+        <button class="deleteBtn" onclick="deleteList(this)">X</button>
+    `;
+
+    listsContainer.appendChild(newList);
+    document.getElementById('ListInput').value = '';
+
+    const deleteBtn = newList.querySelector('.deleteBtn');
+    deleteBtn.onclick = function() {
+        deleteList(this);
+    }
+}
+
+function addItem(button) {
+    const input = button.previousElementSibling;
+    const itemText = input.value;
+    if (itemText === '') {
+        alert('You must write something!');
+        return;
+    }
+
+    const ul = button.parentElement.nextElementSibling;
+    const li = document.createElement('li');
+    li.textContent = itemText;
+    ul.appendChild(li);
+    input.value = '';
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    span.onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+function deleteList(button) {
+    const confirmation = confirm('Are you sure you want to delete this list?');
+    if (confirmation) {
+        const list = button.closest('.container');
+        list.remove();
+    }
 }
